691854e92fd1f3035f034c2b8d0c95af69e456a8
amostinanfon <amostinanfon17@gmail.com>
2023-04-22 19:46:36 +0100

    bugs

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
 import serverAuth from "@/lib/serverAuth";
+
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
@@ -8,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
-        const { currentUser } = await serverAuth(req);
+        const { currentUser } = await serverAuth(req,res);
 
         return res.status(200).json(currentUser);
     } catch (error){
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method != 'POST') {
+    if (req.method !== 'POST') {
         return res.status(450).end();
     }
 
